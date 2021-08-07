1a467b173f83ee58a1c7ffef142a570c7715624e
Tae Kun Kim <taekunkim214@gmail.com>
2021-08-07 14:54:51 -0700

    add

--- BackEnd/app/util/auto_email/auto_email.py
+++ BackEnd/app/util/auto_email/auto_email.py
@@ -4,14 +4,79 @@ Auto Email Sender
 This script allows the user to easily send an email.
 """
 try:
-    import yagmail 
+    import yagmail
+    from yagmail.validate import validate_email_with_regex
+    from yagmail.error import YagAddressError, YagInvalidEmailAddress
+
     yag = yagmail.SMTP('yijianzong@gmail.com', oauth2_file='../../../.env/oauth2_creds.json')
+
 except ImportError:
     raise ImportError('''
                             Please install yagmail\n
                             pip install --user yagmail
                       ''')
 
-def send_email(recipient, subject, body):
-    yag.send(recipient, subject, body)
-    return True
+class FunctionAbusedError(Exception):
+    pass
+
+def __check_abused(ip_address):
+    """
+    :param ip_address: ip address of the user using this function
+    :return: True if the email function has been used more than 5 times in the last minute by "ip_address"
+    """
+
+    # last_request = grab_time(5) # grabs five most recent requests made by the ip_address
+    # time_difference = last_request[1:] - last_request[:-1]
+    # difference_sum = [each.total_seconds for each in time_difference].sum()
+    # return difference_sum < 60
+
+    return False
+
+def __check_overloaded():
+    """
+    :return: True if the email function has been used more than 20 times in the last minute
+    """
+
+    # last_request = grab_time(20) # grabs twenty most recent requests made by the ip_address
+    # time_difference = last_request[1:] - last_request[:-1]
+    # difference_sum = [each.total_seconds for each in time_difference].sum()
+    # return difference_sum < 60
+
+    return False
+
+def send_email(to, subject, content, ip_address, priority=False):
+    """
+    :param to: recipient's email address in string or a list of string addresses
+    :param subject: subject of email
+    :param content: email body text
+    :param ip_address: ip address of the user using this function
+    :param priority: boolean value; if True, suppresses exception
+    :return: True if email was successfully sent, False if not
+
+    Exception raised when invalid data types or email addresses are passed.
+    False returned when email can't be sent.
+    """
+
+    # check parameter type
+    argument_type_check = all([
+        isinstance(to, (list, str)),
+        isinstance(subject, str),
+        isinstance(content, str),
+        isinstance(ip_address, str),
+    ])
+
+    if not argument_type_check:
+        raise ValueError('Invalid parameter type')
+
+    # check if email function is being abused
+    if not priority: # suppress exception if email is important
+        if __check_abused(ip_address):
+            raise FunctionAbusedError('Function has been used too much in the last minute by the ip address')
+            
+        if __check_overloaded():
+            raise FunctionAbusedError('Function has been used too much in the last minute')
+
+    # send email
+    response = yag.send(to, subject, content)  # automatically validates format of addresses contained in "to"
+    return response != False
+    
--- BackEnd/app/util/auto_email/test.py
+++ BackEnd/app/util/auto_email/test.py
@@ -1,3 +1,31 @@
+import unittest
 import auto_email
 
-auto_email.send_email('tkkim@ucsd.edu', 'TEST2', 'This is a test test test test.')
+class Test(unittest.TestCase):
+    def test_invalid_address(self):
+        """
+        checks for invalid email address input
+        """
+        response = auto_email.send_email('invalid_address', 'subject', 'body')
+        self.assertFalse(response)
+
+    def test_multiple_addresses(self):
+        """
+        checks if email can be sent to multiple addresses
+        """
+        response = auto_email.send_email(['tkkim@ucsd.edu', 'taekunkim214@gmail.com'], 'subject2', 'body')
+        self.assertTrue(response)
+
+    def test_bombardment(self):
+        """
+        checks if email bombarding is prohibited
+        """
+
+        self.assertRaises(auto_email.FunctionAbusedError, auto_email.send_email, 'tkkim@ucsd.edu', 'subject', 'content', 'ip_address', False)
+
+    def test_invalid_type(self):
+        self.assertRaises(ValueError, auto_email.send_email, 'tkkim@ucsd.edu', 'test', 'hello', 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
